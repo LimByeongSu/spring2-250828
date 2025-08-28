6a7d644e7c81b24f44f8c9bb3fba07c25e4f7b8d
caketower <dlaqudtn1107@naver.com>
2025-08-28 14:01:59 +0900

    프로젝트 생성

---
+++ src/test/java/com/back/spring2_250828/Spring2250828ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.spring2_250828;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2250828ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
