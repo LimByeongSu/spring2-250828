aa45bb7c4f8ef3ebc48795b8db906458f2efd626
caketower <dlaqudtn1107@naver.com>
2025-08-28 14:19:57 +0900

    [2강] 컨트롤러 2개 생성, PersonService를 2개의 컨트롤러에서 각각 만들어서 사용중

--- src/main/java/com/back/Spring2250828Application.java
+++ src/main/java/com/back/Spring2250828Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring2250828Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring2250828Application.class, args);
+
+        SpringApplication.run(Spring2250828Application.class, args);
 	}
 
 }
