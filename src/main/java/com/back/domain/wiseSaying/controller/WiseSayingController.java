7aff316505e7388fbde256901d0f7996cd8bca1b
caketower <dlaqudtn1107@naver.com>
2025-08-29 14:55:26 +0900

    [18강] delete기능 + @PathVariable사용하여 url에서 값 받아오는 방법

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -3,10 +3,13 @@ package com.back.domain.wiseSaying.controller;
 import com.back.domain.wiseSaying.entity.WiseSaying;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class WiseSayingController {
@@ -16,7 +19,7 @@ public class WiseSayingController {
 
     @GetMapping("/wiseSaying/write")
     @ResponseBody
-    public String write(
+    public String write(        //  /wiseSaying/write? 뒤에 오는 값들을 받아 쓸수있음
             String content,
             String author
     ){
@@ -33,11 +36,43 @@ public class WiseSayingController {
         return wiseSaying.getId()+"번 명언이 등록되었습니다.";
     }
 
-    /*@GetMapping("/wiseSaying/list")
+    @GetMapping("/wiseSaying/list")
     @ResponseBody
-    public String list(){
-        //return wiseSayingList;  //스프링에서 객체를 리턴하면 json형태로 나온다. 물론 보기에 좋지않으니 추천하지않는다.
+    public String list() {
+
+        String wiseSayings = wiseSayingList.stream()
+                .map(w -> "<li>%s / %s / %s</li>".formatted(w.getId(), w.getContent(), w.getAuthor()))
+                .collect(Collectors.joining("\n"));
+
+        return """
+                <ul>
+                %s
+                </ul>
+                """.formatted(wiseSayings);
+    }
+    
+
+    //트랜드를 보면
+    //식별자(id같은)는 /wiseSaying/delete/1
+    //부가정보는 /wiseSaying/delete?author="a"
+    //처럼 하는데 정답은 없으니 그냥 편한거 쓰면 된다.
+
+    @GetMapping("/wiseSaying/delete/{id}")  //식별자니까 /wiseSaying/delete/1의 형태로 받을것이다.
+    @ResponseBody
+    public String delete(
+            @PathVariable int id    // @PathVariable은 path에 있는 값을 꺼내 쓰겠다는 의미다.
+    ){
+        Optional<WiseSaying> wiseSaying=wiseSayingList.stream()
+                .filter(w -> w.getId() ==id)
+                .findFirst();
 
 
-    }*/
+        if(wiseSaying.isEmpty()){
+            throw new RuntimeException(id+"번 명언은 존재하지 않습니다.");
+        }
+
+        wiseSayingList.remove(wiseSaying.get());    //id와 index는 다르기때문에 get()으로 객체를 기반으로 삭제
+
+        return id+"번 명언이 삭제 되었습니다.";
+    }
 }
