53fd925ca1996df8f5735824eb9ec5a66053eccd
caketower <dlaqudtn1107@naver.com>
2025-09-01 08:43:12 +0900

    [22강] WiseSayingRepository 도입

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -5,16 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
-@AllArgsConstructor
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
 
+    public WiseSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
     public void update(String content, String author) {
         this.content = content;
         this.author = author;
+    }
+
+    public boolean isNew() {
+        return this.id == 0;
     }
 }
