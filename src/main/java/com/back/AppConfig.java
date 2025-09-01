584108e0353148d37ed81276a0e2a914b6879722
caketower <dlaqudtn1107@naver.com>
2025-09-01 11:24:02 +0900

    [26강] 재사용성을 위해 파서랑 렌더러를 빈으로 등록해서 사용

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,46 +1,22 @@
 package com.back;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.transaction.annotation.Transactional;
 
 @Configuration  //무언가를 설정하는 클래스에 @Configuration를 붙히는 것이다.
 public class AppConfig {
 
-    @Autowired
-    @Lazy
-    private AppConfig self; //AppConfig이므로 this와 같다고 생각할수있지만 this와는 @Lazy의 차이가 있다.
-
-
-
-    //익명 객체로 만드는 방식 myApplicationRunner1,2는 따로 클래스를 만들어서 구현하고 그걸 받는 방식
     @Bean
-    public ApplicationRunner myApplicationRunner() {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) throws Exception {
-                self.work1();
-                self.work2();
-            }
-        };
+    public Parser parser(){
+        return  Parser.builder().build();
     }
 
-    @Transactional  //this.work()처럼 this로 들어오는 경우는 트랜잭션이 적용되지 않는다.
-    // 그래서 그냥 work1()을 호출하면 this.work1()이니 self를 만들고 self.work1()으로 호출해야한다.
-    private void work1() {
-        System.out.println("work1");
+    @Bean
+    public HtmlRenderer htmlRenderer(){
+        return  HtmlRenderer.builder().build();
     }
-
-    @Transactional
-    private void work2() {
-        System.out.println("work2");
-    }
-
-
 
 
 }
--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -19,6 +19,8 @@ import java.util.stream.Collectors;
 public class WiseSayingController {
 
     private WiseSayingService wiseSayingService;
+    private  final Parser parser;
+    private  final HtmlRenderer renderer;
 
 
     @GetMapping("/wiseSaying/write")
@@ -96,10 +98,8 @@ public class WiseSayingController {
     ){
         WiseSaying wiseSaying = wiseSayingService.findById(id);
 
-        // 파서 & 렌더러 준비
-        Parser parser = Parser.builder().build();
+        // 파서 & 렌더러 준비 -> bean으로 해결
         Node document = parser.parse(wiseSaying.getContent());
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
 
 
         // HTML 변환
